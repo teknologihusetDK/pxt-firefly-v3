4de3e5b4d99acc235aceb1f221f0ea2d30cabcb7
jnielsen-th <77754472+jnielsen-th@users.noreply.github.com>
2022-11-30 09:23:07 +0100

    Testaddition to namespace firefly

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+namespace Firefly {
+
+    export function blink() {
+        pins.P0.digitalWrite(true)
+        pause(500)
+        pins.P0.digitalWrite(false)
+        pause(500)
+    }
+
+}
